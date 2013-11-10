e10c0aa4cb4448f58e226a9ed93666dc8beccf3b
SammyNXS <doctor.hope@knights.ucf.edu>
2013-11-10 11:15:17 -0500

    First Commit:  My Platformer test utilizing a lot of what we've discussed.  Please learn from this.

---
+++ Platformer Test C-Sammy/Assets/Scripts/Music1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music1 : MonoBehaviour {
+	
+	private static Music1 instance = null;
+	
+	
+     
+    public static Music1 Instance {
+    get { return instance; }
+    }
+    void Awake() {
+    if (instance != null && instance != this) {
+    Destroy(this.gameObject);
+    return;
+    } else {
+    instance = this;
+    }
+    DontDestroyOnLoad(this.gameObject);
+    }
+     
+}
---
+++ Platformer Test C-Sammy/Assets/Scripts/NewScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScene : MonoBehaviour {
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Character"){
+			if(Application.loadedLevelName == "PlatformerC_1"){
+				Application.LoadLevel("InsideCastle");	
+			}
+			else{
+				Application.LoadLevel("PlatformerC_1");	
+			}
+		}
+	}
+}
