7f6b0bafdb73ebeee8b43ba2beafa859b49fdf4d
lhigginbotham <lucas.higginbotham@knights.ucf.edu>
2013-11-15 19:52:13 -0500

    Basic Movement for Enemy Tests Added

---
+++ Game Jam Project/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float playerSpeed = 10f;
+	public Vector3 vec;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal") < 0)
+		{
+			Debug.Log("Left");
+			vec = new Vector3(-20f, 0, 0);
+			rigidbody.AddForce(vec);
+		}
+		if(Input.GetAxis("Horizontal") > 0)
+		{
+			Debug.Log("Right");
+			vec = new Vector3(20f, 0, 0);
+			rigidbody.AddForce(vec);
+		}
+		if(Input.GetButtonDown ("Jump"))
+		{
+			Debug.Log ("Space");
+			vec = new Vector3(0, 200f, 0);
+			rigidbody.AddForce(vec);
+		}
+	}
+}
