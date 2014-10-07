478b673e4f4bd1cab0c0a77a44b877abb80825fe
eternus-somnium <clive.hoayun@gmail.com>
2014-10-07 19:41:39 -0400

    Character Control example, Design doc, Map

---
+++ Darkwave/Assets/Character.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+	float jumpCounter = 0.0F;//Used for MoveExample()
+	public float hRotation = 0F, vRotation = 0F;//Used in CameraControlExample()
+	public int health=0, energy=0;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update() 
+	{
+		MoveExample();
+		CameraControlExample();
+	}
+
+	void SimpleMoveExample()
+	{
+		float speed = 3.0F;
+		float rotateSpeed = 3.0F;
+
+		CharacterController controller = GetComponent<CharacterController>();
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+	}
+
+	void MoveExample()
+	{
+		float speed = 6.0F;
+		float jumpSpeed = 8.0F;
+		float jumpPower = 10.0F;
+
+		float gravity = 80.0F;
+		Vector3 moveDirection = Vector3.zero;
+
+		CharacterController controller = GetComponent<CharacterController>();
+
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+
+		if (controller.isGrounded) jumpCounter = jumpPower;
+		else if(!controller.isGrounded && !Input.GetButton("Jump")) jumpCounter = 0;
+		else if(jumpCounter > 0) jumpCounter -= 1*Time.deltaTime;
+
+		if (Input.GetButton("Jump") && jumpCounter > 0) moveDirection.y = jumpSpeed;
+			
+		
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+		
+	}
+
+	void CameraControlExample()
+	{
+		float horizontalSpeed = 7.0F;
+		float verticalSpeed = 7.0F;
+
+		if(!this.GetComponentInChildren<HUD>().getPauseState())
+		{
+			//Rotates Player on "X" Axis Acording to Mouse Input
+			hRotation += horizontalSpeed * Input.GetAxis("Mouse X");
+			transform.localEulerAngles = new Vector3(0, hRotation, 0);
+			
+			//Rotates Player on "Y" Axis Acording to Mouse Input
+			vRotation = Mathf.Clamp(vRotation - verticalSpeed * Input.GetAxis("Mouse Y"), -90,90);
+			Camera.mainCamera.transform.localEulerAngles = new Vector3(vRotation, 0, 0);
+		}
+	}
+
+}
---
+++ Darkwave/Assets/HUD.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour 
+{
+	int i;
+	int j;
+	public Texture2D heartFull;
+	public Texture2D heartNineTenths;
+	public Texture2D heartEightTenths;
+	public Texture2D heartSevenTenths;
+	public Texture2D heartSixTenths;
+	public Texture2D heartFiveTenths;
+	public Texture2D heartFourTenths;
+	public Texture2D heartThreeTenths;
+	public Texture2D heartTwoTenths;
+	public Texture2D heartOneTenths;
+	public Texture2D heartEmpty;
+	
+	static public bool gameOver = false;
+	bool isPaused = false;
+
+	//Starts the game paused.
+	void Start()
+	{
+		Time.timeScale = 0.0f;
+	}
+
+	//This function is called every update.
+	void Update ()
+	{
+		//If the Escape key is pressed, the game will either pause or resume based on the isPaused status.
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(isPaused) resumeGame();
+			else pauseGame();
+		}
+	}
+
+	public bool getPauseState()
+	{
+		return isPaused;
+	}
+
+	//This resumes the game when called.
+	private void resumeGame()
+	{
+		Time.timeScale = 1.0f;
+		isPaused = false;
+	}
+
+	//This pauses the game when called.
+	private void pauseGame()
+	{
+		Time.timeScale = 0.0f;
+		isPaused = true;
+	}
+	
+	//This function is called every update and deals with anything regarding the Graphical User Interface.
+	private void OnGUI()
+	{
+
+		Rect r = new Rect(0,0,Screen.width, Screen.height);
+		GUILayout.BeginArea(r);
+
+		//This statement checks to see if the game is paused. If so, it will display the menu.
+		if (isPaused)
+		{
+			//Menu GUI. Displays only if the game is paused.
+			//Background of menu.
+			GUI.Box (new Rect (440, 250, 120, 60), new GUIContent ("Menu"));
+			//Menu Button 1, Starts or Pauses the game.
+			if (GUI.Button (new Rect (450, 270, 100, 20), new GUIContent ("Start")))
+			{
+				//This will resume the game.
+				resumeGame ();
+			}
+		}
+
+		//Character Information GUI
+		GUILayout.BeginHorizontal();
+		GUILayout.BeginVertical();
+		//GUILayout.Label ("Level " + CharacterSpawner.level);
+		//GUILayout.Label ("Lives " + CharacterSpawner.lives);
+		GUILayout.EndVertical();
+		GUILayout.BeginVertical();
+		for(i=0;i<this.GetComponentInParent<Character>().health;i++) GUI.Box(new Rect (50+i*11,10,2,10), "");
+
+		//Character health
+		if (i == 10)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartFull);
+		}
+		else if (i == 9)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartNineTenths);
+		}
+		else if (i == 8)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartEightTenths);
+		}
+		else if (i == 7)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartSevenTenths);
+		}
+		else if (i == 6)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartSixTenths);
+		}
+		else if (i == 5)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartFiveTenths);
+		}
+		else if (i == 4)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartFourTenths);
+		}
+		else if (i == 3)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartThreeTenths);
+		}
+		else if (i == 2)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartTwoTenths);
+		}
+		else if (i == 1)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartOneTenths);
+		}
+		else if (i == 0)
+		{
+			GUI.Label (new Rect (200, 10, 150, 50), heartEmpty);
+		}
+
+		for(i=0;i<this.GetComponentInParent<Character>().energy;i++) GUI.Box(new Rect (50+i*11,25,2,10), "");
+		GUILayout.Label ("Health " + this.GetComponentInParent<Character>().health);
+		GUILayout.Label ("Energy " + this.GetComponentInParent<Character>().energy);
+		GUILayout.EndVertical();
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal();
+		
+		//Pause GUI
+		if(isPaused)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.Label ("Paused");
+			GUILayout.FlexibleSpace();
+			GUILayout.EndVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+		}
+		
+		//Gameover GUI
+		if(gameOver)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.Label ("GAME OVER");
+			GUILayout.FlexibleSpace();
+			GUILayout.EndVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+		}
+		
+		GUILayout.EndArea();
+	}
+}
