63fe47aa6d0da0de12b12c97edbeafb596042d2b
andrewman986 <andrewguillard@att.net>
2014-10-16 00:04:26 -0400

    AI Test 1.0
    I've added a project in which I have used NavMeshes and AI pathfinding
    to create a very simple enemy who only follows the player.

---
+++ Darkwave/Revenant AI Test 1.0/Pathfinding2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathfinding2 : MonoBehaviour 
+{
+	public Transform destination;
+
+	private NavMeshAgent agent;
+
+	void Start () 
+	{
+
+	}
+
+	void Update()
+	{
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+
+		agent.SetDestination (destination.position);
+	}
+}
