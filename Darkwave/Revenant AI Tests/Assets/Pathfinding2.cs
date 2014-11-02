57408273685a6b3a5c16dbc1403c835c89e48ab8
andrewman986 <andrewguillard@att.net>
2014-11-02 14:08:06 -0500

    AI test 3.0
    AI

---
+++ Darkwave/Revenant AI Tests/Assets/Pathfinding2.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathfinding2 : MonoBehaviour 
+{
+	public Transform destination;
+	private NavMeshAgent agent;
+
+	Transform _player;
+	Transform _transform;
+	float attackRange = 10f;
+
+	void Start () 
+	{
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+
+		_transform = GetComponent<Transform> ();
+		_player = GameObject.Find("Player").GetComponent<Transform>();
+		if (_player == null)
+			Debug.LogError ("No Player on scene.");
+	}
+
+	void Update()
+	{
+		if (RadTest ()) 
+		{
+			agent.SetDestination (_player.position); //Move towards the player.
+		} 
+		else
+		{
+			DefAttack ();
+		}
+
+
+	}
+
+	void DefAttack() //Return to the default attack destination of the enemy. 
+	{
+
+			agent.SetDestination (destination.position); //This is the initial destination of the enemy.
+	}
+
+	bool RadTest() //Tests the radius of the enemy for any players.
+	{
+		if (Vector3.Distance (_player.position, _transform.position) < attackRange) 
+		{
+			print ("Returning True"); //Debuging
+			return true;
+		}
+		else 
+		{
+			print ("Returning False"); //Debuging
+			return false;
+		}
+	}
+}
---
+++ Darkwave/Revenant AI Tests/Assets/Pathfinding3.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathfinding3 : MonoBehaviour 
+{
+	public Transform destination;
+	public double agroDistance;
+	public double attackDistance;
+
+	private Transform target;
+	private NavMeshAgent agent;
+
+	GameObject[] players;
+	Transform _self;
+	
+	void Start ()  //Initialization...
+	{
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+		_self = GetComponent<Transform> ();
+		players = GameObject.FindGameObjectsWithTag ("Player");
+
+		target = destination;
+	}
+	
+	void Update() //Updates the code.
+	{
+		agent.SetDestination (target.position);
+
+		FindPlayer ();
+		double distance = CheckTargetDistance ();
+		if (distance < attackDistance) 
+		{
+			Hit ();
+		} 
+		else
+			agent.speed = 2;
+
+
+	}
+
+
+	void FindPlayer() //Finds players and calls IsAgro to determine if the entity should change targets.
+	{
+		foreach (GameObject player in players)
+		{
+			if(IsAgro(player))
+			{
+				target = player.transform;
+				print ("Agro");
+				return;
+			}
+		}
+
+		target = destination;
+	}
+
+	bool IsAgro(GameObject player) //Does the entity Agro?
+	{
+		if (Vector3.Distance(player.transform.position, _self.position) < agroDistance)
+			return true;
+		else
+			return false;
+	}
+
+	double CheckTargetDistance()
+	{
+		double distance;
+		distance = Vector3.Distance (_self.position, target.position);
+		return distance;
+	}
+
+	void Hit()
+	{
+		agent.speed = 0;
+		//Play "hit" sound
+		//Play "hit" animation
+
+		//agent.speed = 2;
+	}
+}
