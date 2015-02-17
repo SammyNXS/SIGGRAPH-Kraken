0c5726e8caec90c0273ef8cc10a28e0856a4bfa2
Nicoli20 <williamsnsjx@knights.ucf.edu>
2015-02-17 10:27:27 -0500

    APS Build 0.0.1
    Beginning of Attribute Point System

---
+++ Darkwave/Clive's Demos/Assets/Character.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : Entity 
+{
+	float jumpCounter = 0.0F;//Used for MoveExample()
+	float hRotation = 0F, vRotation = 0F;//Used in CameraControlExample()
+
+	//Shot attack variables are in entity
+
+	void Update() 
+	{
+		CameraControlExample();
+		MoveExample();
+		if(Input.GetButton("Fire1")) AttackExample();
+		StatusUpdate();
+	}
+
+	void SimpleMoveExample()
+	{
+		float speed = 3.0F;
+		float rotateSpeed = 3.0F;
+
+		CharacterController controller = GetComponent<CharacterController>();
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+	}
+
+	void MoveExample()
+	{
+		float walkSpeed = 6.0F;
+		float jumpSpeed = 20.0F;
+		float jumpPower = 1.0F;
+
+		float gravity = 500.0F;
+		Vector3 moveDirection = Vector3.zero;
+
+		CharacterController controller = GetComponent<CharacterController>();
+
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= walkSpeed;
+
+		if (controller.isGrounded) 
+		{
+			jumpCounter = jumpPower;
+			moveDirection *= 2;
+		}
+		else if(!controller.isGrounded && !Input.GetButton("Jump")) jumpCounter = 0;
+		else if(jumpCounter > 0) jumpCounter -= 1*Time.deltaTime;
+
+		if (Input.GetButton("Jump") && jumpCounter > 0) moveDirection.y = jumpSpeed;
+			
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+		
+	}
+
+	void CameraControlExample()
+	{
+		float horizontalSpeed = 7.0F;
+		float verticalSpeed = 7.0F;
+
+		//Rotates Player on "X" Axis Acording to Mouse Input
+		hRotation = (hRotation + horizontalSpeed * Input.GetAxis("Mouse X"))%360;
+		transform.localEulerAngles = new Vector3(0, hRotation, 0);
+		
+		//Rotates Player on "Y" Axis Acording to Mouse Input
+		vRotation = Mathf.Clamp(vRotation - verticalSpeed * Input.GetAxis("Mouse Y"), -90,90);
+		Camera.mainCamera.transform.localEulerAngles = new Vector3(vRotation, 0, 0);
+
+	}
+
+	void AttackExample()
+	{
+		shotSpawnPosition = this.gameObject.transform.position;
+		//shotSpawnRotation = gameObject.transform.rotation;
+		shotSpawnRotation = Camera.mainCamera.transform.rotation;
+		Attack();
+	}
+}
---
+++ Darkwave/Clive's Demos/Assets/Shot.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Base code for all shot objects.  All shots disapppear when they move offscreen
+ */
+public class Shot : Entity 
+{
+	
+}
