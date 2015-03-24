d6da98493594e7be01f9dcdba5dbed45fca2dc92
Nicoli20 <williamsnsjx@knights.ucf.edu>
2015-03-24 15:31:13 -0400

    Revert "Consolidated to a Package"
    This reverts commit 5960ad27e2435662cc843ac37b1b67b1cbb09aaf.

---
+++ Darkwave/Clive's Demos/Assets/Scripts/Architect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Architect : Character
+{
+	int Marksmanship = 0;
+	bool HuntersMomentum = false;
+
+	int Structures = 0;
+	int Perception = 0;
+
+	// Use this for initialization
+	void Update()
+	{
+
+	}
+
+	// Called when the player adjusts ability points.
+	void AbilityUnlockMinor( int weapon, int prep, int spirit)
+	{
+		// Sets abilities to true or false depending on how many points each trait line has.
+		if (weapon >= 1)
+		{
+			HuntersMomentum = true;
+		}
+		else
+		{
+			HuntersMomentum = false;
+		}
+	}
+
+	// Called when the level starts
+	void AbilitySet()
+	{
+		if (HuntersMomentum == true)
+		{
+			// Adjusts touch damage of attack1, attack2, attack3, attack4
+		}
+	}
+}
---
+++ Darkwave/Clive's Demos/Assets/Scripts/StraightShot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Code for the straight shots
+public class StraightShot : Shot
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		health = maxHealth;
+		touchDamage = health;
+	}
+	
+	// FixedUpdate is called at a set time interval
+	void FixedUpdate () 
+	{
+		touchDamage = health;
+
+		transform.Translate(new Vector3(0,0,baseSpeed*Time.deltaTime));
+		StatusUpdate();
+	}
+
+	//Controls the shot's behavior when it hits something
+	void OnCollisionEnter(Collision col)
+	{
+		//Player layer object vs enemy layer object collisions
+		if((gameObject.layer == 8) && (col.gameObject.layer == 9) ||
+		   (gameObject.layer == 9) && (col.gameObject.layer == 8))
+		{
+			col.gameObject.GetComponent<Entity>().health -= gameObject.GetComponent<Entity>().health;
+			gameObject.GetComponent<Entity>().health -= col.gameObject.GetComponent<Entity>().touchDamage;
+
+			//If a shot hits anything other than a shot it zeros out it's health.  If a shot hits another shot the weaker one is destroyed
+			if(col.gameObject.tag == "Shot")
+			{
+				if(gameObject.GetComponent<Entity>().health <= col.gameObject.GetComponent<Entity>().health)
+					gameObject.GetComponent<Entity>().health = 0;
+			}
+			else
+				gameObject.GetComponent<Entity>().health = 0;
+
+		}
+		//If a shot hits the terrain it will zero out it's health
+		else if(col.gameObject.layer == 0)
+			gameObject.GetComponent<Entity>().health = 0;
+	}
+
+}
